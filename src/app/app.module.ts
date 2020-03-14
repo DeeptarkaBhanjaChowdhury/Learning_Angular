5473bfdeb5fda70098fdc834c2df5eb724587302
DeeptarkaBhanjaChowdhury <deep.ch1395@gmail.com>
2020-03-14 15:09:06 +0530

    recipe-list created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeaderComponent, RecipesComponent ],
+  declarations: [ AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent, RecipeDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
